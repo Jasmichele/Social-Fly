51308cd17b15fdc203535e699a9e56d63b5dda03
Jasmichele <jasmichele@yahoo.com>
2016-11-30 10:29:28 -0500

    Closing

--- SocialFly/SocialFly/Controllers/ConnectController.cs
+++ SocialFly/SocialFly/Controllers/ConnectController.cs
@@ -3,26 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SocialFly.Models;
 
 namespace SocialFly.Controllers
 {
-    //public class ConnectController : Controller
-    //{
-    //    SocialBEntities db = new SocialBEntities();
-    //    // GET: Connect
-    //    public ActionResult Index()
-    //    {
-    //        return View();
-    //    }
+    public class ConnectController : Controller
+    {
+        SocialBEntities db = new SocialBEntities();
+        // GET: Connect
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-    //    public IEnumerable<SocialUser> GetByFilter(int? rid, int? fid, int? cid)
-    //    {
-    //        var socialUsers = from su in db.SocialUsers
-    //                          select su;
-    //        var filteredUsers = socialUsers;
+        public IEnumerable<SocialUser> GetByFilter(int? rid, int? fid, int? cid)
+        {
+            var socialUsers = from su in db.SocialUsers
+                              select su;
+            var filteredUsers = socialUsers;
 
-    //        if(rid != null)
-    //            filteredUsers = socialUsers.Where<Region>
-    //    }
-    //}
+            if (rid != null)
+                filteredUsers = socialUsers.Where(s => s.RegionId == rid);
+
+            if (fid != null)
+                filteredUsers = socialUsers.Where(s => s.FollowerCountId == fid);
+
+            if (cid != null)
+                filteredUsers = socialUsers.Where(s => s.CompId == cid);
+
+             return filteredUsers;
+        }
+    }
 }
---
+++ SocialFly/SocialFly/Models/ConnectModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialFly.Models
+{
+    public class ConnectModel
+    {
+        public List<Region> Regions { get; set; }
+        public List<Follower> Followers { get; set; }
+        public List<Compensation> Compensation { get; set; }
+    }
+}
